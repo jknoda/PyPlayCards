5137a424f1a0aae549040ab7e133d6943aa9ca82
Jorge Noda <jknoda@hotmail.com>
2021-09-24 10:38:10 -0300

    cors

--- playcardapi.py
+++ playcardapi.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 import pickle
 from flask import Flask, request
+from flask_cors import CORS
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import ElasticNet
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
@@ -12,6 +13,7 @@ from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 model = pickle.load( open('playcardsapi.pkl', 'rb'))
 # instanciate flask
 app = Flask( __name__ )
+CORS(app)
 
 
 @app.route('/')
